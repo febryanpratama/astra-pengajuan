7d98596130d8b603efa71a70923b819486057be4
febryanpratama <spandupan46@student.untan.ac.id>
2023-03-06 14:34:51 +0700

    feat: rev middleware

--- app/modules/vendor/routes/VendorRoutes.js
+++ app/modules/vendor/routes/VendorRoutes.js
@@ -1,6 +1,6 @@
 const VendorController = require("../controllers/VendorController");
 const router = require("express").Router();
-const AuthMiddleware = require("../../../core/middleware/AuthMiddleware");
+// const AuthMiddleware = require("../../../core/middleware/AuthMiddleware");
 const {
   postValidator,
 } = require("../../../core/validators/vendor/VendorValidator");
@@ -8,9 +8,9 @@ const {
 router.get("/", VendorController.findAll);
 
 router.post("/", postValidator, VendorController.store);
-router.get("/:id", AuthMiddleware.AuthAdmin, VendorController.detail);
+router.get("/:id", VendorController.detail);
 router.patch("/:id", postValidator, VendorController.update);
 
-router.delete("/:id", AuthMiddleware.AuthAdmin, VendorController.delete);
+router.delete("/:id", VendorController.delete);
 
 module.exports = router;
