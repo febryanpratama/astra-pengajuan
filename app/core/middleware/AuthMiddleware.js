719a2bbf8f676b8623ed83f1ab5e02542b041ba8
febryanpratama <spandupan46@student.untan.ac.id>
2023-03-18 21:47:36 +0700

    feat: rev auth Middleware

--- app/core/middleware/AuthMiddleware.js
+++ app/core/middleware/AuthMiddleware.js
@@ -6,21 +6,57 @@ class AuthMiddleware {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
+    // console.log(token);
     if (token == null)
       return res.status(401).json({
         status: false,
         message: "Token Not Found",
       });
 
-    jwt.verify(token, "SecretPharse", (err, user) => {
-      console.log(err);
+    let payload = jwt.verify(token, "SecretPharse");
 
-      if (err) return ResponseCode.errorPost(req, res, err);
+    if (payload == null)
+      return res.status(401).json({
+        status: false,
+        message: "Token Not Found",
+      });
 
-      req.user = user;
+    if (payload.roles != "admin") {
+      return res.status(401).json({
+        status: false,
+        message: "Your not Authorized",
+      });
+    }
 
-      next();
-    });
+    next();
+  };
+  AuthUser = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    // console.log(token);
+    if (token == null)
+      return res.status(401).json({
+        status: false,
+        message: "Token Not Found",
+      });
+
+    let payload = jwt.verify(token, "SecretPharse");
+
+    if (payload == null)
+      return res.status(401).json({
+        status: false,
+        message: "Token Not Found",
+      });
+
+    if (payload.roles != "user") {
+      return res.status(401).json({
+        status: false,
+        message: "Your not Authorized",
+      });
+    }
+
+    next();
   };
 }
 
