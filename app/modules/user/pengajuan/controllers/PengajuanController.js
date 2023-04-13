7cfe808a493a0dddc460b731c2534cffdca3ed80
fransiskusagus <fransiskusagus5@gmail.com>
2023-04-13 23:44:11 +0700

    tes tanggal

--- app/modules/user/pengajuan/controllers/PengajuanController.js
+++ app/modules/user/pengajuan/controllers/PengajuanController.js
@@ -240,8 +240,8 @@ exports.report = async (req, res) => {
   // return ResponseCode.successGet(req, res, "Data Pengajuan", data);
   // console.log("data");
   // INI BEDA
-  const startedDate = new Date(data.tanggalmulai + " 00:00:00");
-  const endDate = new Date(data.tanggalselesai + " 23:59:59");
+  const startedDate = new Date(data.tanggal_mulai + " 00:00:00");
+  const endDate = new Date(data.tanggal_selesai + " 23:59:59");
   // return ResponseCode.successGet(req, res, startedDate);
 
   try {
