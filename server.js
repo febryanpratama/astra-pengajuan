be6d64189a8551e134addbc84bf3ef943f40d67c
febryanpratama <spandupan46@student.untan.ac.id>
2023-09-13 11:16:00 +0700

    feat: rev server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const aws = require('aws-sdk');
-const multer = require('multer');
+const aws = require("aws-sdk");
+const multer = require("multer");
 // const multerS3 = require('multer-s3');
 const port = 8080;
 const AuthRoutes = require("./app/modules/authentication/routes/AuthRoutes");
@@ -15,17 +15,37 @@ const PengajuanRoutesadmin = require("./app/modules/admin/pengajuan/routes/Penga
 const PengajuanRoutesatasan = require("./app/modules/atasan/pengajuan/routes/PengajuanRoutes");
 const bodyParser = require("body-parser");
 
-app.use(cors());
-app.use(bodyParser.json({ type: "application/json",limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+var corsOptions = {
+  origin: function (origin, callback) {},
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "device-remember-token",
+    "Access-Control-Allow-Origin",
+    "Origin",
+    "Accept",
+  ],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 aws.config.update({
-  accessKeyId: 'AKIATNRBN2NRZZPT3WGJ',
-  secretAccessKey: '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',
-  region: 'ap-southeast-2',
+  accessKeyId: "AKIATNRBN2NRZZPT3WGJ",
+  secretAccessKey: "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",
+  region: "ap-southeast-2",
 });
-
-
 
 const db = require("./models");
 db.sequelize.sync();
