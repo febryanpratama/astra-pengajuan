302b8090bc035fb89383b423d4a576b92d04da24
febryanpratama <spandupan46@student.untan.ac.id>
2023-09-15 14:35:28 +0700

    feat : rev all data

--- server.js
+++ server.js
@@ -54,32 +54,27 @@ const corsOptions = {
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.header("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.header("Access-Control-Allow-Methods", "*");
-
-  // Request headers you wish to allow
-  res.header("Access-Control-Allow-Headers", "*");
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("preflightContinue", true);
 
   // Pass to next layer of middleware
   next();
 });
 
-const corsOption = {
-  origin: "https://lapor-pak-astra.vercel.app",
-  optionsSuccessStatus: 200,
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, X-Requested-With, Accept, Origin, Referer, User-Agent",
-  preflightContinue: true,
-  credentials: true,
-};
+// const corsOption = {
+//   origin: "https://lapor-pak-astra.vercel.app",
+//   optionsSuccessStatus: 200,
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Headers":
+//     "Content-Type, Authorization, X-Requested-With, Accept, Origin, Referer, User-Agent",
+//   preflightContinue: true,
+//   credentials: true,
+// };
 
-app.use(cors(corsOption));
+// app.use(cors(corsOption));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
